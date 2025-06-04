651d22e37ae28c5a41a0d65078df090cb55a0f3b
Vinimoraes53 <viniciosmoraes53@gmail.com>
2025-06-03 21:20:56 -0300

    !

--- php/listarVagas.php
+++ php/listarVagas.php
@@ -1,12 +1,12 @@
 <?php
 include_once 'db.php';
-session_start();
 
-$id_empresa = $_SESSION['id_usuario'];
 
-$sql = "SELECT * FROM vagas WHERE id_empresa = ?";
+$empresa_id = $_SESSION['id_usuario'];
+
+$sql = "SELECT * FROM vagas WHERE empresa_id = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $id_empresa);
+$stmt->bind_param("i", $empresa_id);
 $stmt->execute();
 $result = $stmt->get_result();
 
